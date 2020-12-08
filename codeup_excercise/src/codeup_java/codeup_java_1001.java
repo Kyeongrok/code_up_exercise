bb6632acd7466cf1055215284a50357bfaefa644
paulham98 <pauljungho.ham@gmail.com>
2020-12-08 20:01:52 +0900

    first commit

---
+++ codeup_excercise/src/codeup_java/codeup_java_1001.java
@@ -0,0 +1,7 @@
+package codeup_java;
+
+public class codeup_java_1001 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
---
+++ codeup_excercise/src/module-info.java
@@ -0,0 +1,2 @@
+module codeup_excercise {
+}
