b5aff07469f916d2f66eb733dc77e4999403f633
paulham98 <pauljungho.ham@gmail.com>
2020-12-11 16:09:11 +0900

    second commit

---
+++ codeup_excercise/src/codeup_java/codeup_java_1011.java
@@ -0,0 +1,14 @@
+package codeup_java;
+
+import java.util.Scanner;
+
+public class codeup_java_1011 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String  x = sc.nextLine();
+		System.out.println(x);
+		sc.close();
+	}
+
+}
