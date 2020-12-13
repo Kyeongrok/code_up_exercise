e52876c99ba829b604005a03725846b149052b9d
paulham98 <pauljungho.ham@gmail.com>
2020-12-13 16:16:22 +0900

    first commit

---
+++ src/codeup/codeup_java_1011.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup_java_1011 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String  x = sc.nextLine();
+		System.out.println(x);
+		sc.close();
+	}
+
+}
---
+++ src/codeup/codeup_java_1020.java
@@ -0,0 +1,17 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup_java_1020 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		a.replace('-' , ' ');
+		System.out.println(a);
+		
+		
+	}
+
+}
