ca4e987557055f0c9e3466360e630440e11682a2
paulham98 <pauljungho.ham@gmail.com>
2021-01-11 20:59:46 +0900

    today commit

--- Jeongho/Programmers1_01.java
+++ Jeongho/Programmers1_01.java
@@ -8,34 +8,43 @@ import java.util.List;
 class Solution {
     public int[] solution(int[] numbers) {
         LinkedList<Integer> list = new LinkedList<Integer>();
-        int cnt = 0;
-        int a=0;
 //        for(int i=1; i<numbers.length;i++) {//더한값을 넣어줄 배열의 길이
 //            a+=i;
 //        }
 //        int[] arr = new int[a];//arr의 크기
-        for(int i = 0; i<numbers.length; i++) {//list에 arr더한값들 넣기
-            for (int j = i+1; j < numbers.length; j++) {
-                list.add(numbers[i]+numbers[j]);
+        boolean bol1 = numbers.length >= 2 && numbers.length <= 100;
+        boolean bol2 = true;
+        if(!bol1) {
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] < 0 || numbers[i] > 100) {
+                    bol2 = false;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {//list에 [i]+[i+1] 넣기
+            for (int j = i + 1; j < numbers.length; j++) {
+                list.add(numbers[i] + numbers[j]);
             }
         }
         System.out.println();//중복 제거
-        for(int i=0; i<list.size();i++) {
-            for(int j=i+1; j<list.size();j++){
-                if(list.get(i)==list.get(j)){
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) == list.get(j)) {
                     list.remove(j);
                 }
             }
         }//오름차순 정렬
         Collections.sort(list);
         //answer에 넣을수 있게 형변환
-        int[] answer = new int[list.size()];
-        for(int i = 0; i<answer.length;i++){
-            answer[i]= list.get(i);
-            System.out.print(answer[i]+" ");
+        int a = list.size();
+        int[] answer = new int[a];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+//            System.out.print(answer[i]+" ");
         }
-            //int[] answer = {};
-            return answer;
+        //int[] answer = {};
+        return answer;
     }
 }
 public class Programmers1_01 {
