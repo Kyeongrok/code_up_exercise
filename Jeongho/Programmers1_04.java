68565a69015ca5efb1c350124f7de79a3b0da043
paulham98 <pauljungho.ham@gmail.com>
2021-01-22 22:02:15 +0900

    add editor

---
+++ Jeongho/Programmers1_04.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+
+public class Programmers1_04 {
+    public static void main(String[] args) {
+        //participant 에서 completion 빼주면 됨
+        //동명이인이 있어도 상관은 participant에서
+        String[] participant = new String[5];
+        String[] completion = new String[5];
+    }
+}
