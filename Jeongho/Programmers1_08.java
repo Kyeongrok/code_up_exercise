7cccb2a20c55a5191796e51858642d5742159635
paulham98 <pauljungho.ham@gmail.com>
2021-01-28 14:53:23 +0900

    2021.1.28

--- Jeongho/Programmers1_08.java
+++ Jeongho/Programmers1_08.java
@@ -1,78 +1,38 @@
+package HelloWorld.Jeongho;
+
 import java.util.*;
 
 class Solution8 {
     public int[] solution(int[] answers) {
-        int[] std1 = new int[answers.length];
-        int[] std2 = new int[answers.length];
-        int[] std3 = new int[answers.length];
-        int a =1;
-        int[] d = {1, 3, 4, 5};
-        int[] f = {3, 1, 2, 4, 5};
-        for(int i = 0; i < answers.length; i++){
-            std1[i] = a;
-            ++a;
-            if(a > 5){
-                a = 1;
+        int[] std1 = {1, 2, 3, 4, 5};
+        int[] std2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] std3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] cnt = {0, 0, 0};
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == std1[i % 5])
+                cnt[0]++;
+            if (answers[i] == std2[i % 8])
+                cnt[1]++;
+            if (answers[i] == std3[i % 10])
+                cnt[2]++;
+        }
+        // 최대값 찾기
+        int max = 0;
+        for (int i = 0; i < 3; i++) {
+            if (cnt[i] > max)
+                max = cnt[i];
+        }
+        ArrayList<Integer> tmp = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            if(cnt[i] == max){
+                tmp.add(i+1);
             }
         }
-        for(int i = 0; i< answers.length; i++){
-            if(i%2 == 0){
-                std2[i]=2;
-            }
-        }
-        for(int i = 0; i<answers.length; i++) {
-            if (i % 2 == 1) {
-                for (int j = 0; j < answers.length/2; j++) {
-                    std2[i] = d[j];
-                    i += 2;
-                }
-            }
-        }
-        for(int i =0; i<answers.length; i++){
-            if(i%2 ==0){
-                for (int j = 0; j < (answers.length/2)+1; j++) {
-                    std3[i] = f[j];
-                    i += 2;
-                }
-            }
-        }
-        for(int i =0; i<answers.length; i++){
-            if(i%2 ==1){
-                for (int j = 0; j < answers.length/2; j++) {
-                    std3[i] = f[j];
-                    i += 2;
-                }
-            }
-        }
-        int cnt1=0;
-        int cnt2=0;
-        int cnt3=0;
-        for(int i =0; i< answers.length; i++){
-            if(answers[i]==std1[i]){
-                cnt1++;
-            }else if(answers[i]==std2[i]){
-                cnt2++;
-            }else if(answers[i]==std3[i]){
-                cnt3++;
-            }
-        }
-        int max = Math.max(cnt1, Math.max(cnt2, cnt3));
+        // 최대값과 같은 수포자 answer에 넣기.
+        int[] answer = new int[tmp.size()];
+        for(int i = 0; i < tmp.size(); i++)
+            answer[i] = tmp.get(i);
 
-        List<Integer> list = new ArrayList<>();
-        if(max == cnt1)
-            list.add(1);
-        if(max == cnt2)
-            list.add(2);
-        if(max == cnt3)
-            list.add(3);
-
-        if(list.size()>1)
-            Collections.sort(list);
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-            System.out.println(answer[i]);
-        }
         return answer;
     }
 }
---
+++ Jeongho/Programmers1_09.java
@@ -0,0 +1,12 @@
+package HelloWorld.Jeongho;
+class Solution9 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+}
+public class Programmers1_09 {
+    public static void main(String[] args) {
+
+    }
+}
