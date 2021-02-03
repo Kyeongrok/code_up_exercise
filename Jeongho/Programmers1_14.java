e1c0809d808afd966f0d4b2989f42512ddc4db92
paulham98 <pauljungho.ham@gmail.com>
2021-02-03 19:12:30 +0900

    2021_2_3

---
+++ Jeongho/Programmers1_14.java
@@ -0,0 +1,13 @@
+class Solution14 {
+    public int solution(int n) {
+        int i = 1;
+
+        int answer = 0;
+        return answer;
+    }
+}
+public class Programmers1_14 {
+    public static void main(String[] args) {
+
+    }
+}
