f373ea6999117389a99231df5a073ebe9e543345
paulham98 <pauljungho.ham@gmail.com>
2021-02-08 16:49:09 +0900

    2021_2_8

---
+++ Jeongho/Programmers1_23.java
@@ -0,0 +1,16 @@
+class Solution23 {
+    public int solution(int n) {
+        for(int i = 1; i <= n; i++){
+            if(n %i== 0) {
+
+            }
+        }
+        int answer = 0;
+        return answer;
+    }
+}
+public class Programmers1_23 {
+    public static void main(String[] args) {
+
+    }
+}
