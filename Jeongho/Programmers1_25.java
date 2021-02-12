434473e7568d24bdb5a6c3113b01accb034560b9
paulham98 <pauljungho.ham@gmail.com>
2021-02-12 23:50:48 +0900

    2021_02_12_2

--- Jeongho/Programmers1_25.java
+++ Jeongho/Programmers1_25.java
@@ -6,7 +6,14 @@ class Solution25 {
         int answer = 0;
 //        String ans = Integer.toString(n);
 //        String[] a = ans.split("");
-        String[] a = String.valueOf(n).split("");
+//        while(n > 0){
+//            answer+=n%10;
+//            if(n<10)
+//                break;
+//
+//            n=n/10;
+//        }
+        String[] a = String.valueOf(n).split("");//가독성은 좋지만 효율성이 떨어짐
         for(int i = 0; i < a.length; i++){
             answer += Integer.parseInt(a[i]);
         }
