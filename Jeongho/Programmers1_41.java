a0eaaa46783914c049730500b2798e6e1cc12c8b
paulham98 <pauljungho.ham@gmail.com>
2021-02-20 21:01:34 +0900

    2021_2_20_ing

--- Jeongho/Programmers1_41.java
+++ Jeongho/Programmers1_41.java
@@ -6,16 +6,19 @@ class Solution41 {
         arr1, arr2 이진수화 해서 그걸 읽어서 1일땐 "#", 0일땐 공백
         중복도 생각
          */
+        String[] ans1 = new String[n];
+        String[] ans2 = new String[n];
         String[] answer = new String[n];
         String a = "";
         String b = "";
-
-
         for(int i = 0; i < arr1.length; i++){
             String c = "";
+            String d = "";
             int id = 0;
+            int id2 = 0;
             a = Integer.toBinaryString(arr1[i]);
-           if(a.length() < 5) {
+            b = Integer.toBinaryString(arr2[i]);
+           if(a.length() < 5) {// make arr1
                while (a.length() + id < 5) {
                    c += "0";
                    id++;
@@ -24,14 +27,31 @@ class Solution41 {
            }else{
                c = a;
            }
-           answer[i] = c;
+           ans1[i] = c;
+            if(b.length() < 5) { // make arr2
+                while (b.length() + id2 < 5) {
+                    d += "0";
+                    id2++;
+                }
+                d += b;
+            }else{
+                d = b;
+            }
+            ans2[i] = d;
         }
-        for(int i = 0; i < answer.length; i++){
+//  하나씩 읽어서 1이면 바꿔주자
+        for(int i = 0; i< n; i++){
+            for(int j = 0;j< 5; j++) {
+                if(ans1[i].charAt(j) == 0 && ans2[i].charAt(j) == 1){
+                    ans1[i]= ans1[i].replace(ans1[i].charAt(j)+"", "1");
+                }
+            }
+            answer[i] = ans1[i];
+            System.out.println(ans1[i] +" "+ ans2[i]);
             System.out.println(answer[i]);
         }
 
-
-        return answer;
+        return ans1;
     }
 }
 public class Programmers1_41 {
