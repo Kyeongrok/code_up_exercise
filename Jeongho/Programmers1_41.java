523259add6b0c781e4dbf69fef94d89f4420d9bb
paulham98 <pauljungho.ham@gmail.com>
2021-02-20 22:44:54 +0900

    2021_2_20

--- Jeongho/Programmers1_41.java
+++ Jeongho/Programmers1_41.java
@@ -18,8 +18,8 @@ class Solution41 {
             int id2 = 0;
             a = Integer.toBinaryString(arr1[i]);
             b = Integer.toBinaryString(arr2[i]);
-           if(a.length() < 5) {// make arr1
-               while (a.length() + id < 5) {
+           if(a.length() < n) {// make arr1
+               while (a.length() + id < n) {
                    c += "0";
                    id++;
                }
@@ -28,8 +28,8 @@ class Solution41 {
                c = a;
            }
            ans1[i] = c;
-            if(b.length() < 5) { // make arr2
-                while (b.length() + id2 < 5) {
+            if(b.length() < n) { // make arr2
+                while (b.length() + id2 < n) {
                     d += "0";
                     id2++;
                 }
@@ -38,28 +38,46 @@ class Solution41 {
                 d = b;
             }
             ans2[i] = d;
+            String.
         }
 //  하나씩 읽어서 1이면 바꿔주자
         for(int i = 0; i< n; i++){
-            for(int j = 0;j< 5; j++) {
-                if(ans1[i].charAt(j) == 0 && ans2[i].charAt(j) == 1){
-                    ans1[i]= ans1[i].replace(ans1[i].charAt(j)+"", "1");
+            String tmp = ans1[i];
+            for(int j = 0;j< n; j++) {
+                if(ans1[i].charAt(j) == '0' && ans2[i].charAt(j) == '1'){
+                    StringBuilder bld = new StringBuilder(ans1[i]);
+                    bld.setCharAt(j,'1');
+                    ans1[i] = bld.toString();
+
                 }
             }
+//            System.out.println(ans1[i]);
             answer[i] = ans1[i];
-            System.out.println(ans1[i] +" "+ ans2[i]);
+//            System.out.println(answer[i]);
+        }
+        for(int i =0; i < answer.length; i++){
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
             System.out.println(answer[i]);
         }
-
-        return ans1;
+//        for(int i = 0; i < n; i++) {
+//            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+//            //비트연산 : 7 | 2 이면 00111 | 00001 일 때 하나라도 1이면 1, 아니면 0
+//        }
+//        for(int i = 0; i < n; i++) {
+//            answer[i] = String.format("%" + n + "s", answer[i]); //n개의 자릿수만큼 문자열 담기
+//            answer[i] = answer[i].replace("1", "#"); //answer[i]가 1이면 #으로 대체
+//            answer[i] = answer[i].replace("0", " "); //answer[i]가 0이면 " "으로 대체
+//        }
+        return answer;
     }
 }
 public class Programmers1_41 {
     public static void main(String[] args) {
         Solution41 sol = new Solution41();
-        int n = 5;
-        int[] arr1 = {9, 20, 28, 18, 11};
-        int[] arr2 = {30, 1, 21, 17, 28};
+        int n = 6;
+        int[] arr1 = {46, 33, 33 ,22, 31, 50};
+        int[] arr2 = {27 ,56, 19, 14, 14, 10};
         sol.solution(n, arr1, arr2);
     }
 }
