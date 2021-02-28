65276ca9eb535df772b5c47f5df3ac0c5a187c2f
paulham98 <pauljungho.ham@gmail.com>
2021-02-28 21:15:43 +0900

    2021_2_28_ing

--- Jeongho/Programmers2_04.java
+++ Jeongho/Programmers2_04.java
@@ -18,19 +18,56 @@ class Solution_4 {
         LinkedList<Integer> list = new LinkedList<>();// index
 
         //첫번째 인쇄를 하고 다음것을 진행 시켜야함
-//        for(int i = 0; i < priorities.length; i++) {//lis에 넣는 과정
-//            lis.add(i, priorities[i]);
-//            System.out.print(lis.get(i));
-//        }
-        Queue<Integer> que1 = new LinkedList<>();// list
-        Queue<Integer> que2 = new LinkedList<>();// index
-        for(int i = 0; i < priorities.length; i++){
-            que1.offer(priorities[i]);
-            que2.offer(i);
+
+        // lis를 기분으로 que1에서 젤 앞에있는거 비교해서 큰게 있으면 빼서 뒤로 보내고
+        // 없으면 그대로 유지
+        // 여기서 1증가 시키고 젤 큰거 빼고
+        // 반복... unitl  location이랑 같게.. 중복된게 있으면..?
+        // que2도 같이 진행시켜서 que2에서 젤 앞에 값이랑 location 이랑 같으면 되잖
+        for(int i = 0; i < priorities.length; i++) {//lis에 넣는 과정
+            lis.add(i, priorities[i]);
+            list.add(i, i);
         }
-        for(int i = 0; i< que1.size(); i++){
+//        Queue<Integer> que1 = new LinkedList<>();// list
+//        Queue<Integer> que2 = new LinkedList<>();// index
+//        int[] arr = new int[priorities.length];
+//        for(int i = 0; i < priorities.length; i++){
+//            que1.offer(priorities[i]);
+//            que2.offer(i);
+//        }
+        int print = 0;//인쇄 할때만
+        while(list.size() > 0){// 다 없어질 때 까지
+                for (int i = 0; i < lis.size(); i++) {//젤 큰 숫자들 거르는 과정
+                    for (int j = i + 1; j < lis.size(); j++) {
+                        if (lis.get(i) < lis.get(j)) { // 앞에것보다 클경우
+                            lis.addLast(lis.removeFirst());
+                            list.addLast(list.removeFirst());
+                            break;
+                        }// 젤 큰숫자가 되면 바로 끊어 줘야 하는데..
+                        if (j == lis.size() - 1) break;
+                    }//for
+                    if (i == lis.size() - 1) break;
+                }//for
+            for(int t =0; t < lis.size(); t++){
+                System.out.print(lis.get(t));
+            }
+            System.out.println();
+                 if(list.get(0) != location) {// 인쇄
+                     lis.removeFirst();
+                     list.removeFirst();
+                     print++;
+                 }
+                 if (list.get(0) == location){
+                    answer = print;
+                    System.out.println(answer);
+                    return answer;
+                 }
+
 
         }
+
+
+/*
         for (int i = 0; i < priorities.length; i++) {
             for (int j = i + 1; j < priorities.length; j++) {
                 if (priorities[i] < priorities[j]) {
@@ -48,7 +85,6 @@ class Solution_4 {
                 lis.add(id, priorities[i]);
                 id++;
             }
-
         }//for
         while(!stack.isEmpty()) {//남은 것들 먼저 들어온 순서대로 들어간다
             int a = stack.poll();
@@ -56,6 +92,7 @@ class Solution_4 {
             lis.add(id, priorities[a]);
             id++;
         }//한번했음
+
 
 
         for (Integer integer : list) {
@@ -85,6 +122,8 @@ class Solution_4 {
         //찾는 과정이 아니라 location이 인쇄될때까지 진행
 
 
+
+ */
         return answer;
     }
 }
